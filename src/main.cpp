5572dfc0833212d9a42c65b821c6fd359315e9a8
Jeff Larson <thejefflarson@gmail.com>
2016-11-19 23:18:36 -0500

    Scanning and displaying networks!

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <ESP8266WiFi.h>
 
 // If using software SPI (the default case):
 #define OLED_MOSI   2
@@ -13,9 +14,7 @@
 #define OLED_RESET 15
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-#if (SSD1306_LCDHEIGHT != 32)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
+
 
 void setup() {
   Serial.begin(9600);
@@ -24,10 +23,22 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("Hi, I'm Jeff!");
+  display.println("Hi Jeff, looking for open wifi networks.");
   display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int n = WiFi.scanNetworks();
+  display.clearDisplay();
+  display.setCursor(0,0);
+  if(n > 0) {
+    display.println("Yo, try these:");
+    for(int i = 0; i < n; i++){
+      display.println(WiFi.SSID(i));
+    }
+  } else {
+    display.println("no networks :(");
+  }
+  display.display();
+  delay(10);
 }
