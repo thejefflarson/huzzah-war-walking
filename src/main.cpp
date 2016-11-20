99839f8da263745a11235dcfcc8d64d52a6f2009
Jeff Larson <thejefflarson@gmail.com>
2016-11-19 22:40:00 -0500

    this->sky

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// If using software SPI (the default case):
+#define OLED_MOSI   2
+#define OLED_CLK   12
+#define OLED_DC    13
+#define OLED_CS    14
+#define OLED_RESET 15
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hi, I'm Jeff!");
+  display.display();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
